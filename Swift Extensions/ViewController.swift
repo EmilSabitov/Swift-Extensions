a2d90d317dd80f70069a3888b53228b65cdc430c
Emil Sabitov <emilsabitov@gmail.com>
2018-07-06 17:07:57 +0300

    Initial Commit

---
+++ Swift Extensions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Extensions
+//
+//  Created by Эмиль Сабитов on 06.07.2018.
+//  Copyright © 2018 Эмиль Сабитов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
